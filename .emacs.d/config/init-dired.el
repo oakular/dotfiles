6d71824468d32754b5b7d92d8e7ecc6abd0a09bb
callumw <callum@oakular.com>
2018-11-11 07:29:21 +0000

    Use better dired-subtree key mappings

--- .emacs.d/config/init-dired.el
+++ .emacs.d/config/init-dired.el
@@ -4,7 +4,14 @@
 (setq dired-async-mode t)
 (setq dired-isearch-filenames t)
 (setq dired-use-ls-dired nil)
-(define-key dired-mode-map  (kbd "i") 'dired-subtree-toggle)
-(setq dired-subtree-use-backgrounds nil)
 
+;; dired subtree org-like keybindings
+(define-key dired-mode-map  (kbd "i") 'dired-subtree-cycle)
+(define-key dired-mode-map  (kbd "C-c C-p") 'dired-subtree-up)
+(define-key dired-mode-map  (kbd "C-c C-n") 'dired-subtree-down)
+(define-key dired-mode-map  (kbd "C-c C-f") 'dired-subtree-next-sibling)
+(define-key dired-mode-map  (kbd "C-c C-b") 'dired-subtree-previous-sibling)
+
+(setq dired-subtree-use-backgrounds nil)
+(setq dired-subtree-cycle-depth 12)
 (provide 'init-dired)
