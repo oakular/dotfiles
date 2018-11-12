a1d6eadd2fb9ae061f9e5e1406c1578c17f591cf
callumw <callum@oakular.com>
2018-11-12 09:05:19 +0000

    Better formatting in init-frame.el

--- .emacs.d/config/init-frame.el
+++ .emacs.d/config/init-frame.el
@@ -5,8 +5,8 @@
 (setq use-file-dialog nil)
 (setq use-dialog-box nil)
 (fboundp 'toggle-frame-fullscreen)
-  ;; Command-Option-f to toggle fullscreen mode
-  ;; Hint: Customize `ns-use-native-fullscreen'
+;; Command-Option-f to toggle fullscreen mode
+;; Hint: Customize `ns-use-native-fullscreen'
 (global-set-key (kbd "M-ƒ") 'toggle-frame-fullscreen)
 (setq initial-frame-alist '((top . 0) (left . 0) (width . 140) (height . 70)))
 (set-face-attribute 'default nil :height 110)
