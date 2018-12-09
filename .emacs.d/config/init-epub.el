cde14708648329794c99a154e2df3edb108cfe3b
callumw <callum@oakular.com>
2018-12-09 14:52:54 +0000

    Emacs: Use Caslon and olivetti-mode for epubs

--- .emacs.d/config/init-epub.el
+++ .emacs.d/config/init-epub.el
@@ -1,9 +1,10 @@
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 (defun my-nov-font-setup ()
-  (face-remap-add-relative 'variable-pitch :family "Literata"
+  (face-remap-add-relative 'variable-pitch :family "Libre Caslon Text"
                                            :height 1.4))
 (add-hook 'nov-mode-hook 'my-nov-font-setup)
 (add-hook 'nov-mode-hook 'variable-pitch-mode)
+(add-hook 'nov-mode-hook 'olivetti-mode)
 (setq nov-text-width 120)
 
 
